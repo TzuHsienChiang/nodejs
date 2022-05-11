a42f7dd13db52493118d03636083aa98128f3450
TzuHsienChiang <happxia@gmail.com>
2022-05-11 13:39:20 +0800

    1-3 練習安裝第三方套件、並且補充coding style（app.js)

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
-//練習印出：console.log("不要再hello world"); 
 
-//第一種方法
-/*
-1.在hello.js定義一個方法叫sayHello()
-2.匯出sayHello這個函式，並且匯出後取名為say(所以在另個檔案要使用時就要用匯出後改得名）：module.exports.say= sayHello;
-3.去app.js匯入了hello模組(hello.js檔案)，並取一個新名、新變數去store匯入內容，匯入完就可以使用它
-const hello=require("./hello") 
-4.呼叫方法：匯入後新名.匯出後新名（當我在app.js呼叫函式，就會出現hello.js匯出的say內容）
-hello.say(); */
+/*補充：coding style 區塊分類寫*/
+// 第一個區塊 內建模組
+const path = require('path');
+const http = require('http');
 
-const hello=require("./hello") //同層 //3.匯入了hello模組(hello.js檔案)，匯入完就可以使用它
-hello.say(); //4.當我執行app.js 就會出現hello.js的內容
+// 第二個區塊 第三方模組(套件)
+// app.js 匯入cowsay、使用它
+const cowsay = require('cowsay');
 
-const greets=require("./hello")
-greets.greetsA();
+console.log(cowsay.say({
+    text : "I'm a moooodule",
+    e : "oO",
+    T : "U "
+}));
 
-//第二種、第三種方法印出資料 
-const hello=require("./hello") //只要匯入一次即可 //hello是接收匯入檔案的所有資料，並store在這hello變量裡面
-console.log(hello.title)//取物件概念：取hello(接受自app.js的所有資料)當中的title
+// 第三個區塊 自建模組
+const hello = require('./hello');
+
+////////////////////////////////////////////////////////////////
+
+hello.sayHello();
+
+console.log(hello.title); 
+
